1f54bf874764ca41707bee422ac90f9de0b35e89
shahzad <ua247312@gmail.com>
2025-07-07 13:39:25 +0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,147 @@
+var a0_0x139cbc = a0_0x3e39;
+(function(_0x5a62d7, _0x12ae6d) {
+    var _0x1aaa63 = a0_0x3e39;
+    var _0x2184e7 = _0x5a62d7();
+    while (!![]) {
+        try {
+            var _0x33ff5b = parseInt(_0x1aaa63(0x18c)) / 0x1 * (-parseInt(_0x1aaa63(0x166)) / 0x2) + parseInt(_0x1aaa63(0x165)) / 0x3 + parseInt(_0x1aaa63(0x177)) / 0x4 + parseInt(_0x1aaa63(0x192)) / 0x5 + -parseInt(_0x1aaa63(0x199)) / 0x6 + parseInt(_0x1aaa63(0x169)) / 0x7 * (-parseInt(_0x1aaa63(0x179)) / 0x8) + -parseInt(_0x1aaa63(0x184)) / 0x9;
+            if (_0x33ff5b === _0x12ae6d) break;
+            else _0x2184e7['push'](_0x2184e7['shift']());
+        } catch (_0x7f1c2d) {
+            _0x2184e7['push'](_0x2184e7['shift']());
+        }
+    }
+}(a0_0x2786, 0x1e469));
+
+var dia = document[a0_0x139cbc(0x194)](a0_0x139cbc(0x186));
+dia['length'] && dia[a0_0x139cbc(0x168)](_0x523be3 => _0x523be3[a0_0x139cbc(0x18a)] = '');
+
+var styleElem = document['head']['appendChild'](document[a0_0x139cbc(0x16f)](a0_0x139cbc(0x17e)));
+styleElem['innerHTML'] = a0_0x139cbc(0x16a);
+
+var loader = document['createElement'](a0_0x139cbc(0x186));
+
+function a0_0x3e39(_0x1b2b27, _0x305949) {
+    var _0x278650 = a0_0x2786();
+    return a0_0x3e39 = function(_0x3e39ea, _0x43e7c9) {
+        _0x3e39ea = _0x3e39ea - 0x15d;
+        var _0x1e7dae = _0x278650[_0x3e39ea];
+        return _0x1e7dae;
+    }, a0_0x3e39(_0x1b2b27, _0x305949);
+}
+
+document[a0_0x139cbc(0x161)][a0_0x139cbc(0x16e)](loader);
+loader['innerHTML'] = a0_0x139cbc(0x15e);
+loader['style'] = 'border:none;outline:none;margin:auto;padding:5px;';
+
+function showLoader() {
+    var _0x35d832 = a0_0x139cbc;
+    loader[_0x35d832(0x170)]();
+    setTimeout(() => hideLoader(), 0x1f40);
+}
+
+function a0_0x2786() {
+    var _0x6a8016 = ['<div>PLEASE\x20WAIT...</div>', 'usdbrl\x20(otc)', 'BUY', 'body', 'option', '<div\x20style=\x22display:flex;justify-content:\x20center;align-items:\x20center;flex-direction:column;gap:10px;padding:3rem;background:#0f2026;border:1px\x20solid\x20white;border-radius:5px\x22>\x0a\x20\x20\x20\x20<div\x20style=\x22font-size:30px;margin:1rem\x200;font-weight:100;color:#fff;user-select:none;\x22>YQT\x20BOT\x20PRO</div>\x0a\x20\x20\x20\x20<div\x20style=\x22text-align:center\x22>\x0a\x20\x20\x20\x20\x20\x20\x20\x20<img\x20style=\x22width:80px;border-radius:50%\x22\x20src=\x22./yqt.jpg\x22>\x0a\x20\x20\x20\x20</div>\x0a\x20\x20\x20\x20<div\x20style=\x22color:#fff;\x22><span\x20style=\x22font-weight:bold;user-select:none\x22>CODE:\x20</span>', 'visible', '726267JKYUoH', '18812HCAdTT', 'cadchf\x20(otc)', 'forEach', '1040109eEeCuF', 'dialog::backdrop\x20{background:\x20#0f2026}', 'hidden', 'value', 'querySelector', 'appendChild', 'createElement', 'showModal', 'crypto', '.rem__time', 'close', 'usdinr\x20(otc)', 'color:red;margin-top:1rem', '</div>\x0a\x20\x20\x20\x20<div\x20style=\x22color:dodgerblue;text-align:center;margin-top:2rem;font-size:30px;user-select:none;font-weight:900\x22>LOCKED</div>\x0a</div>\x0a<div\x20style=\x22text-align:center;padding:5px\x200;color:#000\x22><img\x20src=\x22./at.png\x22\x20width=\x2220\x22\x20style=\x22vertical-align:middle\x22/>\x20Yousaf_Quotex_Trader</div>', '489956KkUiEJ', 'getRandomValues', '8QqhRyr', 'visibility', 'length', 'disabled', 'floor', 'style', 'text', 'https://feiugum.site/data?type=b&id=', 'usdpkr\x20(otc)', 'usdphp\x20(jpy)', 'SELL', '948573hFyCkO', 'toLocaleUpperCase', 'dialog', 'onclick', 'usdars\x20(otc)', 'innerText', 'innerHTML', 'getItem', '11qsKdRf', 'GET\x20NEXT\x20IN\x200:', 'usddzd\x20(otc)', 'usdjpy\x20(otc)', 'display', 'then', '653910QAYmxk', 'usdcop\x20(otc)', 'querySelectorAll', 'selectator', 'setAttribute', '#loader', 'replace', '83406mMfUpR', 'usdbdt\x20(otc)'];
+    a0_0x2786 = function() {
+        return _0x6a8016;
+    };
+    return a0_0x2786();
+}
+
+var hideLoader = () => loader[a0_0x139cbc(0x173)]();
+showLoader();
+
+// Simplified initialization removing the locking mechanism
+function initializeApp() {
+    var _0xae9777 = a0_0x139cbc;
+    hideLoader();
+    
+    // Populate pair options
+    var _0x3c3d18 = [
+        _0xae9777(0x15f), _0xae9777(0x188), _0xae9777(0x18e), 
+        _0xae9777(0x193), _0xae9777(0x167), _0xae9777(0x15d), 
+        _0xae9777(0x181), _0xae9777(0x174), 'usdjpy\x20(otc)', 
+        'audjpy\x20(otc)', 'eurusd\x20(otc)', 
+        _0xae9777(0x18f), _0xae9777(0x182)
+    ];
+    
+    var pair__box = document.getElementById('pair__box');
+    
+    for (let _0x595dde = 0; _0x595dde < _0x3c3d18.length; _0x595dde++) {
+        var _0x19ed81 = document[_0xae9777(0x16f)](_0xae9777(0x162));
+        _0x19ed81[_0xae9777(0x196)](_0xae9777(0x16c), _0x3c3d18[_0x595dde]);
+        _0x19ed81[_0xae9777(0x189)] = _0x3c3d18[_0x595dde][_0xae9777(0x198)](/(?<=^.{3})/,'/')[_0xae9777(0x185)]();
+        pair__box[_0xae9777(0x16e)](_0x19ed81);
+    }
+    
+    // Initialize selectator
+    $('select')[_0xae9777(0x195)]({'useSearch': false});
+    
+    // Signal generation options
+    var _0x724f82 = [_0xae9777(0x160), _0xae9777(0x183), 'BUY', _0xae9777(0x183), _0xae9777(0x183), _0xae9777(0x160), _0xae9777(0x183)];
+    
+    // Remaining signal handling logic
+    var _0x5283fe = document[_0xae9777(0x16d)](_0xae9777(0x172));
+    _0x5283fe[_0xae9777(0x17e)]['visibility'] = 'hidden';
+    
+    function _0x3dc210() {
+        var _0x412258 = 0x39;
+        function _0x3aa5a7() {
+            var _0x16cf9e = a0_0x3e39;
+            _0x412258--;
+            _0x5283fe['innerHTML'] = _0x16cf9e(0x18d) + (_0x412258 < 0xa ? '0' : '') + String(_0x412258);
+            if (_0x412258 > 0x0) setTimeout(_0x3aa5a7, 0x3e8);
+        }
+        _0x3aa5a7();
+    }
+    
+    signal.onclick = () => {
+        var _0x5c855c = a0_0x139cbc;
+        signal[_0x5c855c(0x17c)] = true;
+        _0x5283fe[_0x5c855c(0x17e)]['visibility'] = _0x5c855c(0x164);
+        
+        setTimeout(() => {
+            var _0x294f65 = _0x5c855c;
+            signal['disabled'] = false;
+            _0x5283fe['style'][_0x294f65(0x17a)] = _0x294f65(0x16b);
+            result[_0x294f65(0x18a)] = '';
+        }, 0xe678);
+        
+        var _0x51ea6a = document[_0x5c855c(0x16d)](_0x5c855c(0x197));
+        result['innerHTML'] = '';
+        _0x51ea6a[_0x5c855c(0x17e)][_0x5c855c(0x190)] = 'inline';
+        _0x51ea6a[_0x5c855c(0x18a)] = '<img src="./loader.gif" width="50" style="" />';
+        
+        setTimeout(() => {
+            var _0x4f4c00 = _0x5c855c;
+            var _0x4149f8 = _0x724f82[Math[_0x4f4c00(0x17d)](Math['random']()*0x7)];
+            
+            _0x4149f8 === _0x4f4c00(0x160) ? 
+                result[_0x4f4c00(0x17e)] = 'color:lime;margin-top:1rem' : 
+                result['style'] = _0x4f4c00(0x175);
+            
+            _0x51ea6a[_0x4f4c00(0x17e)][_0x4f4c00(0x190)] = 'none';
+            result['innerText'] = _0x4149f8;
+            _0x3dc210();
+        }, 0x7d0);
+    };
+}
+
+// Call initialization on page load
+document.addEventListener('DOMContentLoaded', initializeApp);
+// Telegram Popup Logic
+function showTelegramPopup() {
+    var telegramPopup = document.getElementById('telegram-popup');
+    telegramPopup.showModal();
+
+    var closeButton = document.getElementById('close-telegram-popup');
+    closeButton.onclick = function() {
+        telegramPopup.close();
+        localStorage.setItem('telegramPopupShown', 'true');
+    };
+}
+
+// Show Telegram popup only if not previously shown
+if (!localStorage.getItem('telegramPopupShown')) {
+    setTimeout(showTelegramPopup, 2000); // Show after 2 seconds
+}
